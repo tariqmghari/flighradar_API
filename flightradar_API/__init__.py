eefea80f9a339e900636802e29d6f7deede09e00
TMGHARI <tariqmghari@gmail.com>
2023-06-25 16:21:43 +0200

    new_changes

--- flightradar_API/__init__.py
+++ flightradar_API/__init__.py
@@ -1,1 +1,5 @@
 from .FRadarAPI import *
+from .airlines import *
+from .airports import *
+from .flights_details import *
+from .requests_handler import *
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Flight_radar_API_unofficial',
+    name='flightradar',
     version='1.0',
     packages=find_packages(),
     install_requires=[
